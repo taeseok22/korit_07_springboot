d557449d84bfbf42076460d0c5d902be9680922b
taeseok22 <dhxotjr12345@naver.com>
2025-09-24 11:10:04 +0900

    feat: RESTful API + Swagger

--- cardatabase/src/main/java/com/example/cardatabase/web/CarController.java
+++ cardatabase/src/main/java/com/example/cardatabase/web/CarController.java
@@ -1,20 +1,20 @@
-package com.example.cardatabase.web;
-
-import com.example.cardatabase.domain.Car;
-import com.example.cardatabase.domain.CarRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CarController {
-    private final CarRepository carrepository;
-
-    public CarController(CarRepository carrepository) {
-        this.carrepository = carrepository;
-    }
-
-    @GetMapping("/cars")
-    public Iterable<Car> getCars() {
-        return carrepository.findAll();
-    }
-}
+//package com.example.cardatabase.web;
+//
+//import com.example.cardatabase.domain.Car;
+//import com.example.cardatabase.domain.CarRepository;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class CarController {
+//    private final CarRepository carrepository;
+//
+//    public CarController(CarRepository carrepository) {
+//        this.carrepository = carrepository;
+//    }
+//
+//    @GetMapping("/cars")
+//    public Iterable<Car> getCars() {
+//        return carrepository.findAll();
+//    }
+//}
